7e039a0501b0ef2e238af6f590cf3782735f3221
Dokgo <deathlife.2k@gmail.com>
2021-05-09 17:10:53 +0700

    BinhBH MyMusic: Function delete song

--- app/src/main/java/com/huybinh2k/mymusic/adapter/SongsAdapter.java
+++ app/src/main/java/com/huybinh2k/mymusic/adapter/SongsAdapter.java
@@ -1,12 +1,20 @@
 package com.huybinh2k.mymusic.adapter;
 
+import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,6 +22,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.huybinh2k.mymusic.R;
 import com.huybinh2k.mymusic.Song;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Locale;
@@ -61,8 +70,28 @@ public class SongsAdapter extends RecyclerView.Adapter<SongsAdapter.ViewHolder> 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm:ss", Locale.ENGLISH);
         String duration = simpleDateFormat.format(song.getDuration());
         holder.time.setText(duration);
+        holder.imageMore.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createPopupMenu(v, song);
+            }
+        });
     }
 
+    private void createPopupMenu(View v, final Song song) {
+        PopupMenu popupMenu = new PopupMenu(mContext, v);
+            popupMenu.getMenuInflater().inflate(R.menu.item_menu, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    if (item.getItemId() == R.id.deleteSong) {
+                        deleteSong(song);
+                    }
+                    return true;
+                }
+            });
+        popupMenu.show();
+    }
 
 
     @Override
@@ -100,6 +129,49 @@ public class SongsAdapter extends RecyclerView.Adapter<SongsAdapter.ViewHolder> 
     }
     public void setOnItemClickListener(OnItemClickListener listener) {
         mListener = listener;
+    }
+
+    /**
+     * @param song to delete
+     * Xóa 1 bài hát
+     */
+    private void deleteSong(final Song song){
+        if (song.getId() == mPlayingId){
+            Toast.makeText(mContext, R.string.cannot_del, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle(R.string.delete_song);
+        builder.setMessage(mContext.getString(R.string.question_delete) + song.getSongName() +" ?");
+        builder.setNegativeButton(R.string.delete, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ContentResolver resolver = mContext.getContentResolver();
+                try {
+                    Uri uri =  MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    String where = MediaStore.Audio.Media._ID + " = " + song.getId();
+                    resolver.delete(uri, where, null);
+                    File fileDelete = new File(song.getSongPath());
+                    if (fileDelete.exists()) {
+                        if (fileDelete.delete()) {
+                            Toast.makeText(mContext, "Delete Success", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    mList.remove(song);
+                    notifyDataSetChanged();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(mContext, "Delete Failure", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder.setPositiveButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.show();
     }
 
 }
